eeda3ae7c0f2a625416c2214fa533df34ec1040b
Rahul_chidambaram <rahul.chidambaram@quietplatforms.com>
2025-03-09 11:56:30 +0530

    firebase config

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -7,13 +7,13 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBY93Bsk3d-aoHpbdiYfUN5nBgByOOhg94",
-  authDomain: "cinibite-13c8e.firebaseapp.com",
-  projectId: "cinibite-13c8e",
-  storageBucket: "cinibite-13c8e.firebasestorage.app",
-  messagingSenderId: "1082386859069",
-  appId: "1:1082386859069:web:3f0fc91fb648f2253fd654",
-  measurementId: "G-VXKXSVDT3Q",
+  apiKey: "AIzaSyBwV_ZEGoGdyfK7dytfKxWmqSly89-xVE0",
+  authDomain: "cini-bite.firebaseapp.com",
+  projectId: "cini-bite",
+  storageBucket: "cini-bite.firebasestorage.app",
+  messagingSenderId: "469448729909",
+  appId: "1:469448729909:web:69799e2a73237b9eb9d87c",
+  measurementId: "G-0BQ5D75L3B",
 };
 
 // Initialize Firebase
